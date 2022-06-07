c870fd99b930bb4f08d7c0e5d69341d2007705b7
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-07 03:11:29 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Code Entry Point
+ * Description - Outputs a sentence using put();
+ * Return: Zero(0)
+ */
+int main(void)
+{
+puts("\n"Programming is like building a multilingual puzzle");
+return(0);
+}
