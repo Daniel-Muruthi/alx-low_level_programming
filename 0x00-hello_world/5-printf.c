194f9210404ca45fa6a9ce317da745266ffba70e
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-07 03:37:50 -0700

    Printing using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * description - Output a string using printf
+  * Return - 0
+  */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+Return (0);
+}
