63aaf0fe0d3b85a85fe6bb83e8b2a26530f885bc
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-07 03:46:14 -0700

    Final commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-  * main - entry point
+  * main - Entry point
   * description - Output a string using printf
   * Return - 0
   */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
