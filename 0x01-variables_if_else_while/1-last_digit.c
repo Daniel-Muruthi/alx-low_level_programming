b7d7811dabc397bb84dc4ab91091204d291ebeb8
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 02:42:56 -0700

    Final Commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,22 +8,22 @@
  */
 int main(void)
 {
-	int n, lastdigit;
+	int n, lastdgt;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastdigit = n % 10;
-	if (lastdigit > 5)
+	lastdgt = n % 10;
+	if (lastdgt > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastdgt);
 	}
-	else if (lastdigit < 6 && lastdigit != 0)
+	else if (lastdgt < 6 && lastdgt != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdgt);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastdgt);
 	}
 	return (0);
 }
