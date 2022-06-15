a66c648b80e1b14d08217351e2fa7bbe6bdd5f40
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 05:22:06 -0700

    Final Commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - printing single digit numbers using putchar
+ *
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
