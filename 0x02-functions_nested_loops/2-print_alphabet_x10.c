a2316c071aab87b3e83ea1d511553ee6b7481819
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 14:20:10 -0700

    Final commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10(void) - prints alphabet 10 times
+ *
+ *
+ *
+ * Return: void
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int x = 0;
+	char c;
+
+	while (x < 10)
+	{
+		for (c = 'a'; c <= 'z'; ++c)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
