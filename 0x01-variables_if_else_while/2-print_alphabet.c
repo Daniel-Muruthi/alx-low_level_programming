c986b0d37365d1b8027cefb72849b56610ced618
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 03:13:02 -0700

    Printing out the alphabet using a loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar("\n");
+	return (0)
+}
