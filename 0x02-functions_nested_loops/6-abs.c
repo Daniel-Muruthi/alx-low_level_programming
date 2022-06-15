c7735a2944d65c657c5da5dba75ff47fc3e11fb3
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 15:36:47 -0700

    Final Commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ *
+ *
+ * Return: 0(success)
+ *
+ */
+int _abs(int)
+{
+	int abs(int);
+	return (0);
+}
