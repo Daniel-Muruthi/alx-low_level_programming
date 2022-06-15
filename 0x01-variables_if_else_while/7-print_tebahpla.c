85a55e3ae92879d92fe5c6e5fdc5c6d3006c689d
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 05:40:45 -0700

    Final Commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Print out the alphabet in reverse
+ *
+ *
+ * Return; 0(success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c => 'a'; --c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
