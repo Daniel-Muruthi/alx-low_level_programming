f0d34fd5327c0b1e6e42ec496dbad1a400cac558
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 13:39:29 -0700

    Final commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -7,7 +8,7 @@
  *
  * Return: 0(success)
  */
-void print_alphabet()
+void print_alphabet(void)
 {
 	char c;
 
