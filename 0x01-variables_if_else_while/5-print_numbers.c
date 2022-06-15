dd0cc324e61a1e2ae09eafbf6fb62d523163b25b
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 05:00:37 -0700

    Final commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - This will print out single digit numbers of base 10
+ *
+ *
+ *Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	printf("\n");
+	return (0);
+}
