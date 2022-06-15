20fe9a71a2468ec5d580844edab7f74d46c94a1e
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 09:56:39 -0700

    Final commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Prints all single digit numbers
+ *
+ *
+ *Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
