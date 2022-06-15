8cc352ad1e3fa534250af0c7666ad81d983aa7f3
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 14:33:23 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * int _islower - checks for lowercase characters
+ *
+ *
+ *
+ * Return: 1 || 0
+ *
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
