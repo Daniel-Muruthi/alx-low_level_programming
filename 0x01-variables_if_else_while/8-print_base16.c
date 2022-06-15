bb256f0211b36e57038fa7e4485a9263c3f50e71
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 09:32:25 -0700

    Final commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ *
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar(i + ('A' - 10));
+	}
+	putchar('\n');
+	return (0);
+}
