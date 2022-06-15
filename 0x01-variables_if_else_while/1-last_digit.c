876dea1ffcab7365ba2334c875c4393b06f52add
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 02:18:29 -0700

    This will add the last digit c code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-  * main - Entry Point
-  *
-  * return: 0(success)
-  */
+ * main - Entry Point
+ *
+ * return: 0(success)
+ */
 int main(void)
 {
 	int n;
@@ -16,13 +16,13 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
-	else if (last_digit == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
-	}
 	else if (last_digit < 6 && last_digit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	return (0);
 }
