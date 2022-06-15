e81af4a7eb9c7eb5679257ac45e42f13392df253
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 12:24:04 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ *
+ * main - print string
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char word = "_putchar";
+
+	putchar(word);
+	putchar('\n');
+	return (0);
+
+}
