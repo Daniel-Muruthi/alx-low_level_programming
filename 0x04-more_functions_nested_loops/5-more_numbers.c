7d29a5b47f14a7063cd14e4841ec9984df34ca4a
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-16 12:11:17 -0700

    Final

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * more_numbers - function prints numbers from 0 - 14 ten times
+ *
+ * Return: void/none
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
