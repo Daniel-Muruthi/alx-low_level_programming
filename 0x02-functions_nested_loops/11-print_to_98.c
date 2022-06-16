8d41c45091b84587fa8d44275db60ddee596cc02
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 20:25:49 -0700

    Final Push

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * print_to_98 - checks for lowercase characters
+ *
+ * @n: parameter
+ *
+ * Return: None/Void
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
