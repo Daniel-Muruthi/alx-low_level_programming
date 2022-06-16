dc95fc4f749bc6bbb262aadaa79f05ddb8ee6dd0
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 20:11:01 -0700

    Adding final changes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * add- adds two integers and returns the result
+ *
+ *
+ *
+ * Return: 0(success)
+ *
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
