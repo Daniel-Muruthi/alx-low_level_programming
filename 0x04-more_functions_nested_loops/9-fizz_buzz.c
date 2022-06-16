f4538fa0b9161f5ed7207127fff099bfc184de69
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-16 15:55:50 -0700

    Final

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int fizzbuzz(void)
+{
+	int i;
+	char a = "Fizz";
+	char b = "Buzz";
+	char c = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			_putchar(a);
+			_putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			_putchar(b);
+			_putchar(' ');
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			_putchar(c);
+			_putchar(' ');
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+}
