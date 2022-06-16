b3bf15109de0e94adb5c85813b381dcb38df5c77
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 18:32:23 -0700

    Final commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,19 +16,15 @@ void times_table(void)
 
 	for (i = 0; i <= 9; ++i)
 	{
-		printf("%d\n", i);
 		for (j = 0; j <= 9; j++)
 		{
 			x = i * j;
 			printf("%d", x);
 			if (j != 9)
 			{
-				printf(", ");
-			}
-			else
-			{
-				break;
+				printf(",\t");
 			}
 		}
 	}
+	printf("\n");
 }
