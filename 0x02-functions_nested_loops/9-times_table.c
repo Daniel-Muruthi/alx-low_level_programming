68e5a42f4bef7ecb7ef4cc93ab7f1a1529144279
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 21:07:41 -0700

    Final

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,15 +22,7 @@ void times_table(void)
 			printf("%d", x);
 			if (j != 9)
 			{
-				printf(",");
-			}
-			if (x < 8)
-			{
-				printf("  ");
-			}
-			else if (x > 7)
-			{
-				printf(" ");
+				printf(", ");
 			}
 		}
 		printf("\n");
