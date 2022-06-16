5df51917f4a765c302b0721d4eb213620671ff0d
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-16 15:24:34 -0700

    Final

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * times_table - Prints the 9 times table.
+ *
+ *
+ *
+ * Return: void(success)
+ *
+ */
+void print_square(int size)
+{
+	int i, j, size;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (j = 0; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
