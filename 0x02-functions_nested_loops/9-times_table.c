38909b62a473a9e6dee5c689f8c1d060c8fd38cf
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-15 23:14:07 -0700

    Final

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 				{
 					printf(" ");
 				}
-				_putchar(n + '0');
+				_putchar(x + '0');
 
 				if (j == 9)
 				{
@@ -45,7 +45,7 @@ void times_table(void)
 				{
 					continue;
 				}
-				_putchar(', ');
+				_putchar(',');
 				_putchar(' ');
 			}
 		}
