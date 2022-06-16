caa812e47c35a1a9548c3600b96ad2b0b80ce40f
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-16 16:58:57 -0700

    Final

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * print_line - function prints straight line
+ * @n: argument
+ * Return: void/none
+ */
+void print_triangle(int size)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,14 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 
 /**
- * main - function prints out the fizzbuzz challenge
+ * main - prints the numbers from 1 to 100, as per the fizzbuzz challenge
  * 
- * Return: (0)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
@@ -17,23 +14,22 @@ int main(void)
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz");
-		}
-		if (i % 5 == 0)
+			printf(" Fizz");
+		} 
+		else if (i % 5 == 0)
 		{
-			printf("Buzz");
-		}
-		if (i % 3 == 0 && i % 5 == 0)
+			printf(" Buzz");
+		} 
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
-		}
+			printf(" FizzBuzz");
+		} 
 		else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
-		if (i != 100)
-		{
-			printf(" ");
 	}
+	printf("\n");
+
 	return (0);
 }
