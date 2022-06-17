8fc07c0ee66afaf7116b3bbb0997c2161cb01c8c
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-16 19:35:45 -0700

    Final

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string and a new line
+ *
+ * @str: pointer to string being printed
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
