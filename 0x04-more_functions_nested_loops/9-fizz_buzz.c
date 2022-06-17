b5d45893c52b84985df276861ab65efa3d4bdee3
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2022-06-16 17:28:18 -0700

    Final

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,27 +2,37 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always (0)
+ * main - function prints out the fizzbuzz challenge
+ * 
+ * Return: (0)
  */
+
 int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100 ; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
+		if (i % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
 		else
+		{
 			printf("%d", i);
-
-		while (i != 100)
-			print(" ");
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
-
 	return (0);
 }
