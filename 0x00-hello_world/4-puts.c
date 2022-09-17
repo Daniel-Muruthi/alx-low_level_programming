8763054cbec8009776b9f5ed016242b781a78773
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-17 05:52:22 -0700

    This will add the 4-puts.c script file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * puts - String printing function
+ *
+ * Return: Always 0 (Success)
+ */
+int puts(void)
+{
+	putchar("Programming is like building a multilingual puzzle");
+	putchar("\n");
+	return (0);
+}
