487b48fe19d20ae167d131852a324a494efe6802
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-17 12:19:39 -0700

    This will add the 3-print_alphabets.c script file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description - Print alphabet in lowercase and uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c, h;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+		for (h = 'A'; h <= 'Z'; ++h)
+		{
+			putchar(h);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
