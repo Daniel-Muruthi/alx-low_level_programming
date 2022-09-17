22268c54a3e73b67d3f5527e99eb9d24fd2db046
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-17 06:16:00 -0700

    This will add the 4-puts.c script file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description - Prints out a sentence followed by a newline
+ * Description - Prints out a sentence followed by a newline using puts()
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
