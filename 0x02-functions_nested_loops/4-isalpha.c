9f244e41f91be854bc87d91fe23fa8396e107aff
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-20 06:05:14 -0700

    This will add the 4-isalpha script file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _isalpha - Entry point
+ * Description - Checks for alphabetic character
+ * @c: character to be checked
+ * Return: Either 1 or 0
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c) == true)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
