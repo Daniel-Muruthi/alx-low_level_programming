0e14134f2588c8141351ad43ea2672bcd37b93be
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-20 05:55:48 -0700

    This will add the 3-islower.c script file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+/**
+ * _islower - Entry point
+ * Description - Function that checks for lowercase character
+ * @c: Character input to be checked
+ * Return: Either 1 or 0 depending on the input
+ */
+int _islower(int c)
+{
+	if (islower(c) == true)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
