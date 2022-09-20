9860d0cadd0c0caa20814fc99bbaafd78810d441
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-20 05:04:27 -0700

    This will add the main.h script file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Description - Prints a string followed by a new line
@@ -8,8 +9,9 @@ int main(void)
 {
 	int i;
 	char c[8] = "_putchar";
+	int j = sizeof(c);
 
-	for (i = 0; i <= (sizeof(c)); i++)
+	for (i = 0; i <= j; i++)
 	{
 		putchar(c + 'a');
 	}
