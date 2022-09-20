ddebee67e7969a564b2ac4856d769c9405b0f74c
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-20 06:30:58 -0700

    This will add the 6-abs.c script file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Description - computes the absolute value of an integer
+ * @int - Value to be absoluted
+ * Return: Absolute value
+ */
+int _abs(int)
+{
+	return (abs(int));
+}
