fee5369e53b913dd50be5cfa8c593d873b71971d
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-21 01:35:24 -0700

    This will add the 10-add.c script file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - Entry point
+ * Description - Adds two integers and returns the results
+ * @i: First integer
+ * @j: Second integer
+ * Return: Returns the summation of the integers
+ */
+int add(int i, int j)
+{
+	int total;
+
+	total = i + j;
+	return (total);
+}
