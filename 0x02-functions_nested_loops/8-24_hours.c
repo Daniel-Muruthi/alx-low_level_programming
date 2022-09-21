03425f05fb6157a2e66900c68754fe71c2b7158b
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-20 23:30:39 -0700

    This will add the 8-24_hours.c script file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - Entry point
+ * Description - Prints every minute of the day of jack Bauer from 00:00-23:59
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 00; i <= 23; i++)
+	{
+		for (j = 00; j <= 59; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
