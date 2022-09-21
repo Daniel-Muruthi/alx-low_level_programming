41e80aa5cc1f48e097d9962d67d2cc92887feca6
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-21 00:40:49 -0700

    This will add the 9-times_table.c script file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,19 +13,35 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			product = (i * j);
-			if (product <= 9 && j < 10)
+			if (product == 0)
 			{
 				_putchar(product + '0');
+				if (j == 9)
+					continue;
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 			}
-			else if (product > 9 && j < 10)
+			else
 			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (product < 10)
+				{
+					_putchar(product + '0');
+					if (j == 9)
+						continue;
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+					if (j == 9)
+						continue;
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
