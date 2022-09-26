a485139e0cf7dce637a25553a403ae4a5688bdac
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-26 09:08:52 -0700

    This will add the 103-fibonacci.c script file

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,26 +7,24 @@
  */
 int main(void)
 {
-	long int i[33], limit, count, sum;
+	long int i, j, limit, count, sum;
 
 	sum = 0;
 	limit = 4000000;
-	i[0] = 1;
-	i[1] = 2;
-	for (count = 2; count < 34; count++)
+	i = 1;
+	j = 2;
+	count = 0;
+	while (1)
 	{
-		i[count] = i[(count - 1)] + i[(count + 2)];
-		if (i[count] % 2 == 0)
+		sum = i + j;
+		if (sum > limit)
+			break;
+		if (sum % 2 == 0)
 		{
-			sum = sum + i[count];
-			if (i[count] > limit)
-			{
-				break;
-			}
-			else
-				printf("%ld", sum);
+			count = count + sum;
 		}
 	}
+	printf("%ld", sum);
 	printf("\n");
 	return (0);
 }
