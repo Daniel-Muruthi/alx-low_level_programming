ac2274ad65cb83572292150139ef6b01934a976b
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-26 14:57:29 -0700

    This will add the 2-mul.c script file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * mul - Entry point
+ * Description - Multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Returns product of a and b
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
