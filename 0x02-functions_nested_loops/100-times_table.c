93455641bbfeff0797188bc8e17ce2935756da09
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-26 00:35:19 -0700

    This will add the 100-times_table.c script file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,7 @@ void print_times_table(int n)
 					}
 					else
 					{
-						if (product >= 90 && j == (n-1)
+						if (product >= 90 && j == (n-1))
 						{
 							printf("%d", product);
 							printf(", ");
