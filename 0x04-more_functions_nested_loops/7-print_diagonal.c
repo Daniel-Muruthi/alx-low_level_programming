685ed34e1f63e3855a29379f4900cbb604fb9365
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-27 00:01:08 -0700

    This will add the 7-print_diagonal file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,13 +6,15 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
 		i = 1;
 		while (i <= n)
 		{
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
 			i += 1;
