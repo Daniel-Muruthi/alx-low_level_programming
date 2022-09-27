870fd7d74180fc4cd83e04a27ba5a5d86a0478ab
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-09-27 00:47:19 -0700

    This will add the 10-print_triangle.c file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle - Entry point
+ * Description - Prints a triangle followed by a newline
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
