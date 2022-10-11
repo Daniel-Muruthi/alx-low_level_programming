cc00ce2648d7b7602688f625668e5b1eba595dd4
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-10-11 07:20:29 -0700

    This will ass the 3-puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - Entry Point
+ * Description - Prints a string followed by a newline
+ * @str: string variable
+ */
+void _puts(char *str)
+{
+	puts(*str);
+}
