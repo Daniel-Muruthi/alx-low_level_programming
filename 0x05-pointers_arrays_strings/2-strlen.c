d66254ac9e600f5c98e155983833a7568f186d72
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-10-11 04:13:15 -0700

    This will add the 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strlen - Entry point
+ * Description - Returns the length of a string
+ * @s: string element
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = strlen(*s);
+	return (i);
+}
