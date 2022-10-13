b7cecb43613e1963138f1d29884c0b30102e7a86
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-10-13 09:32:40 -0700

    This will add the 100-atoi.c file

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,9 +10,13 @@
 int _atoi(char *s)
 {
 	int i = atoi(s);
+	int j;
 
-	if (s == "-")
-		return ((-1 * i));
-	else
-		return (i);
+	for (j = 0; j < strlen(s); j++)
+	{
+		if (s[j] == "-")
+			return ((-1 * i));
+		else
+			return (i);
+	}
 }
