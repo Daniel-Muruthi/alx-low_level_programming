0348af272e4360fe40380fa79ba860b913563247
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-10-13 08:24:31 -0700

    This will add the 9-strcpy.c file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - Entry Point
+ * Destination - Copies string from source to destination
+ * @dest: Destination
+ * @src: Source
+ * Return: string copied
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
