41f6a828c3030b195ac735e4260ec866cca7ed68
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-10-13 07:07:59 -0700

    This will add the 7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ *
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (str[j++])
+	{
+		k++;
+	}
+	if (k % 2 == 0)
+	{
+		for (i = k/2; i <= (k - 1); i++)
+		{
+			printf("%c", str[i]);
+		}
+		printf("\n");
+	}
+	if (k % 2 != 0)
+	{
+		for (i = (k - 1)/2; i <= (k - 1); i++)
+		{
+			printf("%c", str[i]);
+		}
+		printf("\n");
+	}
+}
