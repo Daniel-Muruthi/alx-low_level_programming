582af111e1ed5367143b3e8390896b4b3a3b421a
Daniel-Muruthi <adinomuruthi1@gmail.com>
2022-10-13 05:11:59 -0700

    This will add the 6-puts2.c script file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,10 @@
+#include <string.h>
+/**
+ * puts2 - Entry point
+ * Description - Prints all characters of a string followed by a new line
+ * @str: string printed
+ */
+void puts2(char *str)
+{
+	puts(str);
+}
